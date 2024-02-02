261787fcb1a90a3e1ccf7988553040befd821503
Alexander Gladkov <gladkov481@gmail.com>
2024-02-02 22:57:43 +0700

    Navigation in ViewModel added

---
+++ BankAccount/BankAccount/Command/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BankAccount.Command
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _Execute(parameter);
+      
+        
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }  
+}
---
+++ BankAccount/BankAccount/ViewModel/MainBankAccViewModel.cs
@@ -0,0 +1,75 @@
+﻿using BankAccount.Command;
+using BankAccount.Service;
+using BankAccount.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Popups;
+
+namespace BankAccount.ViewModel
+{
+    public class MainBankAccViewModel : ViewModelBase
+    {
+
+        public MainBankAccViewModel() 
+        {
+            NavigateToAddTrancCommand = new RelayCommand(OnNavigateToAddTrancCommandExecuted, CanNavigateToAddTrancCommandExecute);
+
+            NavigateToHistoryCommand = new RelayCommand(OnNavigateToHistoryCommandExecuted, CanNavigateToHistoryCommandExecute);
+        }
+
+
+        public ICommand NavigateToAddTrancCommand { get; }
+        private bool CanNavigateToAddTrancCommandExecute(object parameter) => true;
+        private void OnNavigateToAddTrancCommandExecuted(object parameter)
+        {
+            NavigateToAddTranc();
+        }
+
+        public void NavigateToAddTranc()
+        {
+            try
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                rootFrame.Navigate(typeof(AddTransactionPage));
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog($"Ошибка перехода. Код ошибки: {ex.Message}", "Уведомление");
+                dialog.ShowAsync();
+            }
+        }
+
+        public ICommand NavigateToHistoryCommand { get; }
+        private bool CanNavigateToHistoryCommandExecute(object parameter) => true;
+        private void OnNavigateToHistoryCommandExecuted(object parameter)
+        {
+            NavigateToHistory();
+        }
+
+        public void NavigateToHistory()
+        {
+            try
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                rootFrame.Navigate(typeof(HistoryPage));
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog($"Ошибка перехода. Код ошибки: {ex.Message}", "Уведомление");
+                dialog.ShowAsync();
+            }
+        }
+
+
+
+
+
+    }
+}
