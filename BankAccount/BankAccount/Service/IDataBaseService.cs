eae8c8fab1732763f57eff2a7feaa952fd0a9727
Alexander Gladkov <gladkov481@gmail.com>
2024-02-05 01:54:26 +0700

    Add Transaction done
    Добавление транзакций полностью работает, не хватает грамотной проверки валидации данных (стоит заглушка)

--- BankAccount/BankAccount/Service/IDataBaseService.cs
+++ BankAccount/BankAccount/Service/IDataBaseService.cs
@@ -13,8 +13,10 @@ namespace BankAccount.Service
     public interface IDataBaseService
     {
         Task InitializeDatabaseAsync();
-        Task<List<Transaction>> GetTransactionsAsync();
         Task UpdateCurrencyTableAsync();
+        Task<List<Transaction>> GetTransactionsAsync();
+        Task<List<string>> GetCurrencyNamesAsync();
+        Task AddTransactionAsync(decimal amount, string currency, string transactionType);
     }
 
 
@@ -54,7 +56,8 @@ namespace BankAccount.Service
                     SqliteCommand insertTransactionCommand = connection.CreateCommand();
                     insertTransactionCommand.CommandText = @"
                         INSERT INTO Transactions (DateTime, Amount, Currency, Type)
-                        VALUES ('2024-02-03', 100.00, 'USD', 'Deposit')";
+                        VALUES ('2024-02-03', 100.00, 'USD', 'Deposit'),
+                               ('2024-02-03', 100.00, 'USD', 'Deposit')";
                     await insertTransactionCommand.ExecuteNonQueryAsync();
 
 
@@ -83,38 +86,7 @@ namespace BankAccount.Service
                 }
             }
         }
-
-        public async Task<List<Transaction>> GetTransactionsAsync()
-        {
-            List<Transaction> transactions = new List<Transaction>();
-
-            string dbPath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, dbFileName);
-
-            using (var connection = new SqliteConnection($"Data Source={dbPath}"))
-            {
-                await connection.OpenAsync();
-
-                var selectCommand = connection.CreateCommand();
-                selectCommand.CommandText = "SELECT * FROM Transactions";
-
-                using (var reader = await selectCommand.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        Transaction transaction = new Transaction
-                        {
-                            OperationID = reader.GetInt32(0),
-                            DateTime = DateTime.Parse(reader.GetString(1)),
-                            Amount = reader.GetDecimal(2),
-                            Currency = reader.GetString(3),
-                            Type = reader.GetString(4)
-                        };
-                        transactions.Add(transaction);
-                    }
-                }
-            }
-            return transactions;
-        }
+        
 
         public async Task UpdateCurrencyTableAsync()
         {
@@ -192,6 +164,106 @@ namespace BankAccount.Service
             catch (Exception ex)
             {
                 throw new Exception("Ошибка при обновлении валюты: " + ex.Message, ex);
+            }
+        }
+
+        public async Task<List<Transaction>> GetTransactionsAsync()
+        {
+            List<Transaction> transactions = new List<Transaction>();
+
+            string dbPath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, dbFileName);
+
+            using (var connection = new SqliteConnection($"Data Source={dbPath}"))
+            {
+                await connection.OpenAsync();
+
+                var selectCommand = connection.CreateCommand();
+                selectCommand.CommandText = "SELECT * FROM Transactions";
+
+                using (var reader = await selectCommand.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        Transaction transaction = new Transaction
+                        {
+                            OperationID = reader.GetInt32(0),
+                            DateTime = DateTime.Parse(reader.GetString(1)),
+                            Amount = reader.GetDecimal(2),
+                            Currency = reader.GetString(3),
+                            Type = reader.GetString(4)
+                        };
+                        transactions.Add(transaction);
+                    }
+                }
+            }
+            return transactions;
+        }
+
+
+        public async Task<List<string>> GetCurrencyNamesAsync()
+        {
+            List<string> currencyNames = new List<string>();
+
+            string dbPath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, dbFileName);
+
+            using (var connection = new SqliteConnection($"Data Source={dbPath}"))
+            {
+                await connection.OpenAsync();
+
+                var selectCommand = connection.CreateCommand();
+                selectCommand.CommandText = "SELECT DISTINCT CharCode FROM Currency";
+
+                using (var reader = await selectCommand.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        string currencyName = reader.GetString(0);
+                        currencyNames.Add(currencyName);
+                    }
+                }
+            }
+
+            return currencyNames;
+        }
+
+
+        public async Task AddTransactionAsync(decimal amount, string currency, string transactionType)
+        {
+            try
+            {
+                DateTime currentDate = DateTime.Now;
+
+                using (var connection = new SqliteConnection($"Data Source={dbFileName}"))
+                {
+                    await connection.OpenAsync();
+                    var transaction = connection.BeginTransaction();
+
+                    try
+                    {
+                        var insertTransactionCommand = connection.CreateCommand();
+                        insertTransactionCommand.CommandText = @"
+                            INSERT INTO Transactions (DateTime, Amount, Currency, Type)
+                            VALUES ($dateTime, $amount, $currency, $type)";
+
+                        insertTransactionCommand.Parameters.AddWithValue("$dateTime", currentDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                        insertTransactionCommand.Parameters.AddWithValue("$amount", amount);
+                        insertTransactionCommand.Parameters.AddWithValue("$currency", currency);
+                        insertTransactionCommand.Parameters.AddWithValue("$type", transactionType);
+
+                        await insertTransactionCommand.ExecuteNonQueryAsync();
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw new Exception("Ошибка при добавлении транзакции: " + ex.Message, ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при обращении к базе данных: " + ex.Message, ex);
             }
         }
     }
---
+++ BankAccount/BankAccount/ViewModel/AddTransactionViewModel.cs
@@ -0,0 +1,133 @@
+﻿using BankAccount.Command;
+using BankAccount.Model;
+using BankAccount.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BankAccount.ViewModel
+{
+    public class AddTransactionViewModel : ViewModelBase
+    {
+        private readonly IDataBaseService _dataBaseService;
+        public AddTransactionViewModel(IDataBaseService dataBaseService)
+        {
+            _dataBaseService = dataBaseService;
+
+            _ = GetAllComboBoxNames();
+
+            AddTransactionCommand = new RelayCommand(OnAddTransactionCommandExecuted, CanAddTransactionCommandExecute);
+
+            NavigateBackToMainCommand = new RelayCommand(OnNavigateBackToMainCommandExecuted, CanNavigateBackToMainCommandExecute);
+        }
+
+
+        
+        public ObservableCollection<string> TransactionTypes { get; private set; }
+        
+        private string _selectedTransactionType;
+        public string SelectedTransactionType
+        {
+            get { return _selectedTransactionType; }
+            set { Set(ref _selectedTransactionType, value); }
+        }
+
+
+        
+        private ObservableCollection<string> _currencyNames;
+        public ObservableCollection<string> CurrencyNames
+        {
+            get { return _currencyNames; }
+            set { Set(ref _currencyNames, value); }
+        }
+
+        private string _selectedCurrencyName;
+        public string SelectedCurrencyName
+        {
+            get { return _selectedCurrencyName; }
+            set { Set(ref _selectedCurrencyName, value); }
+        }
+
+
+        
+        private string _transactionAmount;
+        public string TransactionAmount
+        {
+            get { return _transactionAmount; }
+            set { Set(ref _transactionAmount, value); }
+        }
+
+
+        private async Task GetAllComboBoxNames()
+        {
+            TransactionAmount = "";
+
+            TransactionTypes = new ObservableCollection<string>
+            {
+                "Зачисление",
+                "Снятие"
+            };
+
+            SelectedTransactionType = TransactionTypes.FirstOrDefault();
+
+            List<string> currencyNamesFromDatabase = await _dataBaseService.GetCurrencyNamesAsync();
+
+            CurrencyNames = new ObservableCollection<string>(currencyNamesFromDatabase);
+
+            SelectedCurrencyName = CurrencyNames.FirstOrDefault();
+        }
+
+
+
+        public ICommand AddTransactionCommand { get; }
+        private bool CanAddTransactionCommandExecute(object parameter) => true;
+
+        private async Task OnAddTransactionCommandExecuted(object parameter)
+        {
+            try
+            {
+                decimal amount = decimal.Parse(TransactionAmount);
+                await _dataBaseService.AddTransactionAsync(amount, SelectedCurrencyName, SelectedTransactionType);
+                await NavigateBackToMain();
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog($"Ошибка ввода суммы. Код ошибки: {ex.Message}", "Уведомление");
+                await dialog.ShowAsync();
+            }
+        }
+
+        
+
+
+        public ICommand NavigateBackToMainCommand { get; }
+        private bool CanNavigateBackToMainCommandExecute(object parameter) => true;
+        private async Task OnNavigateBackToMainCommandExecuted(object parameter)
+        {
+            await NavigateBackToMain();
+        }
+
+        public async Task NavigateBackToMain()
+        {
+            try
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                if (rootFrame.CanGoBack)
+                {
+                    rootFrame.GoBack();
+                }
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog($"Ошибка перехода назад. Код ошибки: {ex.Message}", "Уведомление");
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
